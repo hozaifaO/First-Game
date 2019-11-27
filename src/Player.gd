2058396d0992de10932cfdf1dea4e3af2a6941e4
Hozaifa Owaisi <hozaifaowaisi.h@gmail.com>
2019-11-27 16:07:43 -0500

    added a new feature to inturupt a jump

--- src/Player.gd
+++ src/Player.gd
@@ -1,9 +1,10 @@
 extends Actor
-
 func _physics_process(delta):
-	
+	var is_jump_interupted = Input.is_action_just_released("jump") and velocity.y < 0.0
 	velocity.x = input_direction().x * SPEED
 	velocity.y += GRAVITY
+	if is_jump_interupted:
+		velocity.y = 0.0
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump(velocity)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)

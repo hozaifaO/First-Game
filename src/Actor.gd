697cd51f3cf0bd273957a97e5caffd8b56a440de
Hozaifa Owaisi <hozaifaowaisi.h@gmail.com>
2019-11-29 10:28:48 -0500

    Added some stuff to the enemy

--- src/Actor.gd
+++ src/Actor.gd
@@ -3,7 +3,7 @@ class_name Actor
 
 
 const FLOOR_NORMAL: = Vector2.UP
-export var jump_impulse = 1000
+export var jump_impulse = 1200
 export var SPEED: = 350
 export var GRAVITY: = 60
 
@@ -11,4 +11,4 @@ var velocity: = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += GRAVITY 
+	velocity.y += GRAVITY *delta
--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,11 +1,19 @@
 extends Actor
-var direction = 1.0
 func _ready():
-	pass
-
-func _physics_process(delta):
-	velocity.x = -SPEED * direction
-	velocity = move_and_slide(velocity,FLOOR_NORMAL)
+	set_physics_process(false)
+	velocity.x = -SPEED 
 	
+func _on_Stop_Detector_body_entered(body):
+	
+	if body.global_position.y > get_node("Stop Detector").global_position.y:
+		return
+	queue_free()
+	
+func _physics_process(delta):
+	if is_on_floor():
+		velocity.y = 0
 	if is_on_wall():
-		direction = direction * -1.0
+		velocity.x *= -1.0
+	velocity.y = move_and_slide(velocity,FLOOR_NORMAL).y
+
+
